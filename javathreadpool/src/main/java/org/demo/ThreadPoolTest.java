084bcb5dd2d6f7a5efe95307155149b6e3dc288d
Nuwan Wimalasekara <nuwanw@wso2.com>
2017-11-10 18:23:41 +0530

    Add logs

--- javathreadpool/src/main/java/org/demo/ThreadPoolTest.java
+++ javathreadpool/src/main/java/org/demo/ThreadPoolTest.java
@@ -23,9 +23,10 @@ import java.util.concurrent.Executors;
 public class ThreadPoolTest {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(5);//creating a pool of 5 threads
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             Runnable worker = new MyWorkerThread("Task" + i);
             executor.execute(worker);//calling execute method of ExecutorService
+            System.out.println("Task Added");
         }
         executor.shutdown();
         while (!executor.isTerminated()) {
