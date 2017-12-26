8e312bfd7850a75bfc7d94f9955929e6fbebeff9
Nuwan Wimalasekara <nuwanw@wso2.com>
2017-12-26 16:36:45 -0600

    Refcator class

--- dev-studio-projects/ESB-Security/CallbackHandler/src/main/java/org/demo/PWCallbackHandler.java
+++ dev-studio-projects/ESB-Security/CallbackHandler/src/main/java/org/demo/PWCallbackHandler.java
@@ -3,20 +3,16 @@ package org.demo;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
-
 import org.apache.ws.security.WSPasswordCallback;
-
 import java.io.IOException;
-public class PWCallbackHandler implements CallbackHandler {
 
+public class PWCallbackHandler implements CallbackHandler {
 	
 	public void handle(Callback[] callbacks) throws IOException,
 			UnsupportedCallbackException {
 
 		for (int i = 0; i < callbacks.length; i++) {
-
 			WSPasswordCallback pwcb = (WSPasswordCallback) callbacks[i];
-
 			int usage = pwcb.getUsage();
 			String id = pwcb.getIdentifier();
 
@@ -30,6 +26,5 @@ public class PWCallbackHandler implements CallbackHandler {
 				} 
 			}
 		}
-
 	}
 }
