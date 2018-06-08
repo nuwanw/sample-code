0aa89d0fa9d2038493e03c3fb008f442cbdd0d28
Nuwan Wimalasekara <nuwanw@Nuwans-MacBook-Pro-2.local>
2018-06-08 12:21:19 +0530

    Update data source pooling sample

--- pooling-sample/src/main/java/org/demo/DatabaseConnectionPooling.java
+++ pooling-sample/src/main/java/org/demo/DatabaseConnectionPooling.java
@@ -11,46 +11,72 @@ public class DatabaseConnectionPooling {
 
     public static void main(String[] args) {
         BasicDataSource mysqlDataSource = new BasicDataSource();
-        mysqlDataSource.setUrl("jdbc:mysql://localhost:3306/dbname");
+        mysqlDataSource.setUrl("jdbc:mysql://localhost:3306/testdb");
         mysqlDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        mysqlDataSource.setUsername("username");
-        mysqlDataSource.setPassword("password");
+        mysqlDataSource.setUsername("root");
+        mysqlDataSource.setPassword("root123");
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new SQLQueryWorker(mysqlDataSource));
+            thread.start();
+        }
+
         try {
-            connection = mysqlDataSource.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT id,name FROM EMPLOYEE");
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println("Employ ID " + resultSet.getString("id") + " Name " + resultSet.getString("name"));
-            }
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            mysqlDataSource.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    //ignore
+        }
+    }
+
+    private static class SQLQueryWorker implements Runnable {
+        private BasicDataSource mysqlDataSource;
+
+        SQLQueryWorker(BasicDataSource mysqlDataSource) {
+            this.mysqlDataSource = mysqlDataSource;
+        }
+
+        public void run() {
+            Connection connection = null;
+            PreparedStatement preparedStatement = null;
+            ResultSet resultSet = null;
+            try {
+                connection = mysqlDataSource.getConnection();
+                preparedStatement = connection.prepareStatement("SELECT id,name FROM EMPLOYEE");
+                resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    System.out
+                            .println("Employ ID " + resultSet.getString("id") + " Name " + resultSet.getString("name"));
                 }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    //ignore
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (resultSet != null) {
+                    try {
+                        resultSet.close();
+                    } catch (SQLException e) {
+                        //ignore
+                    }
                 }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    //ignore
+                if (preparedStatement != null) {
+                    try {
+                        preparedStatement.close();
+                    } catch (SQLException e) {
+                        //ignore
+                    }
+                }
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        //ignore
+                    }
                 }
             }
         }
-
     }
 }
