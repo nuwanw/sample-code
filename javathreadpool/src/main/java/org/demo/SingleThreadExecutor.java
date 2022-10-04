e135efb46a194e39a87b567a25f2b920ac210c24
Nuwan Wimalasekara <nuwanw@nuwanw.local>
2022-10-04 19:44:16 +0530

    Add single thread executor

--- javathreadpool/src/main/java/org/demo/SingleThreadExecutor.java
+++ javathreadpool/src/main/java/org/demo/SingleThreadExecutor.java
@@ -1,2 +1,14 @@
-package org.demo;public class SingleThreadExecutor {
+package org.demo;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutor {
+    public static void main(String[] args) {
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> System.out.println(Thread.currentThread().getName() + " Hello World"));
+        executor.execute(() -> System.out.println(Thread.currentThread().getName() + " Hello World"));
+        System.exit(0);
+
+    }
 }
